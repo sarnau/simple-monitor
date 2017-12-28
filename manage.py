bfe2db609880aec05ee4168a0763ef20b5fca30e
Markus Fritze <github@sarnau.com>
2017-12-28 13:13:55 +0100

    Add MQTT Listener

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ from app.models import Hosts
 from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 from config import config
+from mqtt import mqtt
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -34,7 +35,6 @@ def setup():
         db.create_all()
         print('Database created!')
         print('You can start the server by running "gunicorn -c gunicorn_conf.py manage:app"')
-
 
 if __name__ == '__main__':
     manager.run()
